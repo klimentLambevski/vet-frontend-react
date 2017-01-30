b8c582b8482066fea9aadfe334a84a835fcb94be
korun <korun_stefan@hotmail.com>
2017-01-30 09:53:22 +0100

    merge

--- client/app/routes.jsx
+++ client/app/routes.jsx
@@ -1,12 +1,11 @@
-import { Route, IndexRoute } from "react-router";
-import App from './components/app';
-import LoginPage from './components/auth/login-page';
-import HomePage from './components/home/home';
-import DashboardPage from './components/dashboard/dashboard.page';
-import RequireAuthentication from './hocs/require-authentication';
+import {IndexRoute, Route} from "react-router";
+import App from "./components/app";
+import LoginPage from "./components/auth/login-page";
+import HomePage from "./components/home/home";
+import DashboardPage from "./components/dashboard/dashboard-page";
+import CustomersPage from "./components/dashboard/customer/customers-page";
+import PatientsPage from "./components/dashboard/patients/patients-page";
 import {PatientsContainer} from "./views/patients";
-import CustomersPage from './components/dashboard/customer/customers-page';
-import PatientsPage from './components/dashboard/patients/patients-page';
 
 export default (
   <Route path="/" component={App}>
