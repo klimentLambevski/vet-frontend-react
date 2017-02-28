91a9c4f35d9841b4699710326bf45c141fe02546
korun <korun_stefan@hotmail.com>
2017-03-01 00:50:46 +0100

    feat(grid) copy to clipboard --unfinished

--- client/app/components/grid/grid-bricks.jsx
+++ client/app/components/grid/grid-bricks.jsx
@@ -1,6 +1,8 @@
+import {TextField} from 'material-ui';
+import Clipboard from 'clipboard';
+
 let THead = ({columns, rows, searchChanged}) => {
   let cols = columns || _.keys(rows[0]);
-
   return (
     <thead>
     <tr>{
@@ -8,7 +10,8 @@ let THead = ({columns, rows, searchChanged}) => {
     }</tr>
     <tr>{
       _.map(cols, (col, key) =>
-        <th className="row-search"><input onChange={(e) => searchChanged(key, e.target.value)}/></th>
+        <th className="row-search"><TextField hintText="пребарај" onChange={(e) => searchChanged(key, e.target.value)}/>
+        </th>
       )
     }</tr>
     </thead>
@@ -18,13 +21,29 @@ let THead = ({columns, rows, searchChanged}) => {
 let TBody = ({columns, rows, _onRowClick}) => {
   let cols = columns; // || _.keys(rows[0]); enable columns based on rows object keys
 
+  let clipboard = new Clipboard('.overlay-hover', {
+    text: (trigger) => {
+      return '123';
+    }
+  });
+  console.log('copyToClipboard',  clipboard.text());
+
+  function copyToClipboard(proxy) {
+    proxy.stopPropagation();
+  }
+
   return (
     <tbody>{
       rows.map((row) => (
         <tr onClick={() => _onRowClick(row)}>{
           _.map(cols, (conf, name) => {
             let cell = _.get(row, name);
-            return <td>{_.isObject(cell) ? JSON.stringify(cell) : cell}</td>;
+            return <td>
+              {_.isObject(cell) ? JSON.stringify(cell) : cell}
+              <div className="overlay-hover" onClick={(event) => event.stopPropagation()}>
+                копирај
+              </div>
+            </td>;
           })
         }</tr>
       ))
